81465cd9cf5f67fbabbbe1f4f0150a86c39adea9
Sachin <sachin.san2131@gmail.com>
2022-12-29 14:49:03 +0100

    added cloud config and sample service to test changes

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -3,14 +3,21 @@
  */
 package com.example.accounts.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.accounts.config.AccountsServiceConfig;
 import com.example.accounts.model.Accounts;
 import com.example.accounts.model.Customer;
+import com.example.accounts.model.Properties;
 import com.example.accounts.repository.AccountsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 /**
  * @author User
@@ -21,6 +28,9 @@ public class AccountsController {
 	
 	@Autowired
 	AccountsRepository accountsRepository;
+	
+	@Autowired
+	AccountsServiceConfig accountsConfig;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -33,4 +43,15 @@ public class AccountsController {
 		
 		return null;
 	}
+	
+	
+	@GetMapping("/account/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+
 }
--- loans/src/main/java/com/example/loans/controller/LoanController.java
+++ loans/src/main/java/com/example/loans/controller/LoanController.java
@@ -3,19 +3,28 @@ package com.example.loans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.loans.config.LoansServiceConfig;
 import com.example.loans.model.Customer;
 import com.example.loans.model.Loans;
+import com.example.loans.model.Properties;
 import com.example.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class LoanController {
 	
 	@Autowired
 	private LoansRepository loansRepository;
+	
+	@Autowired
+	private LoansServiceConfig loansServiceConfig;
 	
 	@PostMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -28,4 +37,12 @@ public class LoanController {
 
 	}
 
+	@GetMapping("/loan/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansServiceConfig.getMsg(), loansServiceConfig.getBuildVersion(),
+				loansServiceConfig.getMailDetails(), loansServiceConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
 }
