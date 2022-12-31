46c041945d3cf0066f6617480125fac3a52ecb6f
Sachin <sachin.san2131@gmail.com>
2022-12-31 14:01:38 +0100

    added example of feign client

--- accounts/src/main/java/com/example/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/example/accounts/controller/AccountsController.java
@@ -4,6 +4,8 @@
 package com.example.accounts.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.accounts.config.AccountsServiceConfig;
 import com.example.accounts.model.Accounts;
+import com.example.accounts.model.Cards;
 import com.example.accounts.model.Customer;
+import com.example.accounts.model.CustomerDetails;
+import com.example.accounts.model.Loans;
 import com.example.accounts.model.Properties;
 import com.example.accounts.repository.AccountsRepository;
+import com.example.accounts.service.client.CardsFeignClient;
+import com.example.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -27,10 +34,16 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 public class AccountsController {
 	
 	@Autowired
-	AccountsRepository accountsRepository;
+	private AccountsRepository accountsRepository;
 	
 	@Autowired
-	AccountsServiceConfig accountsConfig;
+	private AccountsServiceConfig accountsConfig;
+	
+	@Autowired
+	private CardsFeignClient cardsFeignClient;
+	
+	@Autowired
+	private LoansFeignClient loansFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -52,6 +65,25 @@ public class AccountsController {
 				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
+	}
+	
+	@PostMapping("/myAccountDetails")
+	public CustomerDetails getCustomerDetails(@RequestBody Customer customer) {
+		Accounts account = accountsRepository.findByCustomerId(customer.getCustomerId());
+		if(account == null) {
+			return null;
+		}
+		
+		List<Loans> loansDetails = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cardDetails = cardsFeignClient.getCardDetails(customer);
+		
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(account);
+		customerDetails.setCards(cardDetails);
+		customerDetails.setLoans(loansDetails);
+		
+		return customerDetails;   
+		
 	}
 
 }
